62ae8fb769ef84e20f5039ad6ae3fa6bdf172f3d
shayma972003 <ttom9977.td@gmail.com>
2025-11-06 20:45:23 -0500

    Revert "new line"
    This reverts commit 41c58b24c9cc521b8e83f1b479376eff9aae4963.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,4 +10,3 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
  # Here is a new update by shayma972003
- # Here is another update by <your GitHub username>
